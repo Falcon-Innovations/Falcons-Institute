9d0c2c32139008aaba6ed7b5c5d54c8ab881aaee
Eldrige <peldrige8@gmail.com>
2023-04-24 14:05:16 +0100

    style:create the navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,13 @@
 
 import {Fragment} from "react";
-import {ParallaxProvider} from 'react-scroll-parallax';
-import {Hero} from "@/components/home/hero";
-import {HowItWorks} from '@/components/home/how-it-works'
-import {WhyChooseUs} from "@/components/home/why-choose-us";
-import {WhyJoinUs} from "@/components/home/why-join-us";
-import {Footer} from "@/components/shared/footer";
-import {Faq} from "@/components/home/faq";
-import {WhatTalentsSay} from "@/components/home/what-talents-say";
+import Navbar from "@/components/shared/navbar";
 
 export default function Home() {
   return (
     <Fragment>
-      <ParallaxProvider>
-        <Hero />
-        <HowItWorks />
-        <WhyChooseUs />
-        <WhyJoinUs />
-        <WhatTalentsSay />
-        <Faq />
-        <Footer />
-      </ParallaxProvider>
+      <main className="container mx-auto px-16 2xl:px-0">
+        <Navbar />
+      </main>
     </Fragment>
   );
 }
